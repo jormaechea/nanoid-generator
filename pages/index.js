079ce7bd539e23c0981d967e776a8417a06aa4e7
Joaquín Ormaechea <joaquin@fizzmod.com>
2021-05-09 18:21:02 -0300

    Fixed GA lib dir

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useMemo, useState } from 'react'
 import styles from '../styles/Home.module.css'
-import { EVENTS, logEvent } from './lib/analytics';
+import { EVENTS, logEvent } from '../lib/analytics';
 
 const minLength = 3;
 const maxLength = 40;
